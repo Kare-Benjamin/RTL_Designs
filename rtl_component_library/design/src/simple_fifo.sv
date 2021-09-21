7cc7f092869075cb6bd7ad566c106da60172bcb6
kare-benjamin h rorvik <kbhrorvik@gmail.com>
2021-09-21 17:09:20 +0200

    Added pointers and enables to the logic, still more to be done but getting there.1

--- rtl_component_library/design/src/simple_fifo.sv
+++ rtl_component_library/design/src/simple_fifo.sv
@@ -1,28 +1,48 @@
 // Code your design here
 
-module FIFO (data_out, data_in, reset, clk, write_enable, write_pointer, read_enable, read_pointer);
+module FIFO (data_out, data_in, reset, clk, write_enable, read_enable);
     output logic [7:0] data_out;
     input logic [7:0] data_in;
     input logic reset;
     input logic clk;
     input logic write_enable;
-    input logic write_pointer;
     input logic read_enable;
-    input logic read_pointer;
+    
     
     /* Memory for temporary storage of FIFO data */
     logic [7:0] memory [7:0];
+  
+  	/* Pointers */
+  	logic [7:0] write_pointer;
+ 	logic [7:0] read_pointer;
+    
+    /*
+    generate genvar i;
+        for (i = 0; i < 8; i++) begin
+            // Do nothing
+        end
+    endgenerate
+    */
     
     always @(posedge clk) begin
-      if (reset) 
-        begin
-            data_out <= 8'b00000000;
-        end 
-      else if (write_enable)
-        begin
-          memory[write_pointer] <= data_in;
-          write_pointer++;
-        end
-      else if (read_enable)
+        if (reset) 
+            begin
+                data_out <= 8'b00000000;
+                write_pointer <= 8'b0000_0000;
+                read_pointer <= 8'b0000_0000;
+                    for (int i = 0; i < 8; i++) begin
+                        memory[i] = 8'b0000_0000;
+                    end       
+            end 
+        else if (write_enable)
+            begin
+                memory[write_pointer] <= data_in;
+                write_pointer++;
+            end
+        else if (read_enable)
+            begin
+                data_out[read_pointer] <= memory[read_pointer];
+                read_pointer++;
+            end
     end
-  endmodule
+endmodule
--- rtl_component_library/design/tb/simple_fifo_tb.sv
+++ rtl_component_library/design/tb/simple_fifo_tb.sv
@@ -1,28 +1,31 @@
 module FIFO_tb;
     logic [7:0] data_out;
     logic [7:0] data_in;
-    logic reset, clk, write_enable, write_pointer, read_enable, read_pointer;
-    
+    logic reset, clk, write_enable, read_enable;
+
     FIFO FIFO_DUT(.*);
-    
+
     /* Waveform Dump */
     initial begin 
-      $dumpfile("dump.vcd"); $dumpvars;
+        $dumpfile("dump.vcd"); $dumpvars;
     end
-    
+
     initial begin
-      $display("We are starting");
+    $display("We are starting");
+        reset = 1;
         clk = 0;
-      data_in = 8'b00110011;
-      #10;
-      clk = 1;
-      data_in = 8'b11001100;
-      #10;
-      clk = 0;
-      data_in = 8'b00110011;
-      #10;
-      clk = 1;
-      data_in = 8'b00001111;
-      #10;
+        write_enable = 1'b1;
+        data_in = 8'b00110011;
+        #10;
+        reset = 0;
+        clk = 1;
+        data_in = 8'b11001100;
+        #10;
+        clk = 0;
+        data_in = 8'b00110011;
+        #10;
+        clk = 1;
+        data_in = 8'b00001111;
+        #10;
     end
-  endmodule
+endmodule
