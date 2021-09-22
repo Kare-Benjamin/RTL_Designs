a1af61de6b6e8970a6ce063e8e5297c546a84be2
kare-benjamin h rorvik <kbhrorvik@gmail.com>
2021-09-22 20:38:47 +0200

    First functional build of the simple FIFO.

--- rtl_component_library/design/tb/simple_fifo_tb.sv
+++ rtl_component_library/design/tb/simple_fifo_tb.sv
@@ -1,8 +1,20 @@
 module FIFO_tb;
-    logic [7:0] data_out;
-    logic [7:0] data_in;
-    logic reset, clk, write_enable, read_enable;
+    /* FIFO parameters */
+    parameter FIFO_DEPTH = 8;
+    parameter FIFO_W = 8;
 
+    /* Simulation parameters */
+    parameter PERIOD = 5;
+
+    /* Outputs */
+    logic [FIFO_DEPTH - 1:0] data_out;
+    logic empty, full;
+
+    /* Inputs */
+    logic [FIFO_DEPTH - 1:0] data_in;
+    logic rst, clk, write_enable, read_enable;
+
+    /* Instantiations */
     FIFO FIFO_DUT(.*);
 
     /* Waveform Dump */
@@ -10,22 +22,37 @@ module FIFO_tb;
         $dumpfile("dump.vcd"); $dumpvars;
     end
 
+    /* clock setup */
+    always #PERIOD clk =! clk;
+
     initial begin
-    $display("We are starting");
-        reset = 1;
-        clk = 0;
-        write_enable = 1'b1;
-        data_in = 8'b00110011;
-        #10;
-        reset = 0;
+        $display("FIFO test is starting");
+
+        /* Setup and reset stage */
         clk = 1;
+        rst = 1; write_enable = 0; read_enable = 0; 
+        data_in = 8'b00110011;
+        #10; rst = 0; write_enable = 0; read_enable = 0; 
         data_in = 8'b11001100;
-        #10;
-        clk = 0;
+      
+      	/* write to FIFO */
+      	#10; rst = 0; write_enable = 1; read_enable = 0; 
+        data_in = 8'b11001100;
+        #10; rst = 0; write_enable = 1; read_enable = 0; 
         data_in = 8'b00110011;
-        #10;
-        clk = 1;
+        #10; rst = 0; write_enable = 1; read_enable = 0; 
         data_in = 8'b00001111;
+
+         /* Read from FIFO */
+        #10; rst = 0; write_enable = 0; read_enable = 1; 
+        data_in = 8'b11111111;
+        #10; rst = 0; write_enable = 0; read_enable = 1; 
+        data_in = 8'b11111111;
+        #10; rst = 0; write_enable = 0; read_enable = 1; 
+        data_in = 8'b11111111;
+
+        /* Simulation complete */
         #10;
+        $finish;
     end
 endmodule
